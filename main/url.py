d613ec03eec323b8364708e57d26c9ce12ab7582
hollayemi18 <olayemio521@gmail.com>
2023-06-09 03:04:41 +0100

    committed

--- main/url.py
+++ main/url.py
@@ -4,6 +4,5 @@ from main import views
 
 urlpatterns = [
     path('', views.show, name="show"),
-    path('emp', views.emp),
-    path('map?p<id>/', views.user_map, name="map")
+    path('emp', views.emp)
 ]
--- main/views.py
+++ main/views.py
@@ -1,31 +1,6 @@
 from django.shortcuts import render, redirect
-from geopy.geocoders import Nominatim
 from .models import Address
 from .forms import StoreData
-
-# map function
-
-
-def user_map(request, id):
-    try:
-        # getting adress from database
-        user = Address.objects.get(id=id)
-
-        stre = user.street
-        city = user.city
-        # concant adress from the database
-        address = f"{user.street}, {user.city}, {user.country} "
-        # calling the nominatim server
-        geolocator = Nominatim(user_agent="my_application")
-        # nominatim working it magic
-        location = geolocator.geocode(address)
-        lat = location.latitude
-        lng = location.longitude
-        return render(request, 'user_map.html', {'lat': lat, 'lng': lng, 'user': user, 'street': stre, 'city': city})
-    except AttributeError:
-        return redirect('/emp')
-
-# just a form to save data
 
 
 def emp(request):
@@ -45,5 +20,6 @@ def emp(request):
 
 
 def show(request):
-    storedata = Address.objects.all()
-    return render(request, "show.html", {'storedata': storedata})
+    user = Address.objects.get(id=1)
+
+    return render(request, "show.html", {'user': user})
